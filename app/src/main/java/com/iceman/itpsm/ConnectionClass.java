79128c83119f2a6cbd9c021ab44c40b8e59a7783
jktmuhip <boskovic234@gmail.com>
2016-12-26 17:35:21 +0700

    Update Kantor 1

---
+++ app/src/main/java/com/iceman/itpsm/ConnectionClass.java
@@ -0,0 +1,46 @@
+package com.iceman.itpsm;
+
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by jktmuhip on 12/26/2016.
+ */
+
+public class ConnectionClass {
+    String IP = "172.16.0.73";
+    String classs = "net.sourceforge.jtds.jdbc.Driver";
+    String DB = "ITProduction";
+    String UN = "sa";
+    String PASS = "AdIns2009";
+
+
+    @SuppressLint("NewApi")
+    public Connection CONN() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection conn = null;
+        String ConnURL = null;
+        try {
+
+            Class.forName(classs);
+            ConnURL = "jdbc:jtds:sqlserver://" + IP + ";"
+                    + "databaseName=" + DB + ";user=" + UN + ";password="
+                    + PASS + ";";
+            conn = DriverManager.getConnection(ConnURL);
+        } catch (SQLException se) {
+            Log.e("ERRO", se.getMessage());
+        } catch (ClassNotFoundException e) {
+            Log.e("ERRO", e.getMessage());
+        } catch (Exception e) {
+            Log.e("ERRO", e.getMessage());
+        }
+        return conn;
+    }
+}
--- app/src/main/java/com/iceman/itpsm/MainActivity.java
+++ app/src/main/java/com/iceman/itpsm/MainActivity.java
@@ -1,6 +1,8 @@
 package com.iceman.itpsm;
 
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v4.media.MediaBrowserServiceCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,14 +11,29 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class MainActivity extends AppCompatActivity {
 
     String username;
     String password;
     EditText ETUsername;
     EditText ETPassword;
     Button BTNLogin;
+    ConnectionClass koneksi;
+    String querylogin;
+
+    //DatabaseParameter
+    String IDK;
+    String NameK;
+    String PositionK;
+    String PhotoK;
+    String MobilePhoneK;
+    String PasswordK;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +48,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void login(View view) {
 
-        Intent intentlayarutama = new Intent(this, LayarUtama.class);
-
         username = ETUsername.getText().toString();
         password = ETPassword.getText().toString();
 
@@ -41,9 +56,105 @@ public class MainActivity extends AppCompatActivity {
         } else if (password == null || password.isEmpty()) {
             Toast.makeText(this, "Please Insert Password !", Toast.LENGTH_SHORT).show();
         } else {
-            Log.d("Disni", username);
-            intentlayarutama.putExtra("Login", username);
-            startActivity(intentlayarutama);
+
+            DoLogin doLogin = new DoLogin();
+            doLogin.execute("");
+       }
+    }
+
+    public void wrongpassword()
+    {
+        Toast.makeText(this, "Password not match", Toast.LENGTH_SHORT).show();
+    }
+
+    public void matchpassword()
+    {
+        Intent intentlayarutama = new Intent(this, LayarUtama.class);
+        Log.d("Disni", username);
+        intentlayarutama.putExtra("Login", username);
+        startActivity(intentlayarutama);
+    }
+
+    public class DoLogin extends AsyncTask<String, String, String> {
+        String z = "";
+
+
+        @Override
+        protected String doInBackground(String... params) {
+            Connection kon = koneksi.CONN();
+            Log.d("Disini","Disiniii");
+            if (kon == null) {
+                z = "Error koneksi SQL";
+            } else {
+                querylogin = "select ID," +
+                        "Name," +
+                        "Position," +
+                        "Photo," +
+                        "MobilePhone," +
+                        "Password " +
+                        "from Master_PIC " +
+                        "where DI =" + "'" + username + "'";
+
+                Statement stmt = null;
+                Log.d("Disini",querylogin);
+
+                try {
+                    stmt = kon.createStatement();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    ResultSet RS = stmt.executeQuery(querylogin);
+
+                    if (RS.next()) {
+                        StringBuilder ID = new StringBuilder();
+                        StringBuilder Name = new StringBuilder();
+                        StringBuilder Position = new StringBuilder();
+                        StringBuilder Photo = new StringBuilder();
+                        StringBuilder MobilePhone = new StringBuilder();
+                        StringBuilder Password = new StringBuilder();
+
+                        String ID1 = RS.getString(1);
+                        String Name1 = RS.getString(2);
+                        String Position1 = RS.getString(3);
+                        String Photo1 = RS.getString(4);
+                        String MobilePhone1 = RS.getString(5);
+                        String Password1 = RS.getString(6);
+
+                        ID.append(ID1);
+                        Name.append(Name1);
+                        Position.append(Position1);
+                        Photo.append(Photo1);
+                        MobilePhone.append(MobilePhone1);
+                        Password.append(Password1);
+
+                        IDK = ID.toString();
+                        NameK = Name.toString();
+                        PositionK = Position.toString();
+                        PhotoK = Photo.toString();
+                        MobilePhoneK = MobilePhone.toString();
+                        PasswordK = Password.toString();
+
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            return z;
+        }
+
+        protected void  onPostExecute(String result)
+        {
+            if (password != PasswordK)
+            {
+                wrongpassword();
+            }
+            else
+            {
+                matchpassword();
+            }
         }
     }
 }
