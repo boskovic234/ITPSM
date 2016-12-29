edfed3462edb9ee6d22ce470c83384e141b48ffa
Boskovic <boskovic234@gmail.com>
2016-12-29 23:20:07 +0700

    Perbaikan menu, Rumah

--- app/src/main/java/com/iceman/itpsm/LayarUtama.java
+++ app/src/main/java/com/iceman/itpsm/LayarUtama.java
@@ -2,8 +2,10 @@ package com.iceman.itpsm;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 /**
@@ -15,6 +17,11 @@ public class LayarUtama extends AppCompatActivity {
     TextView TVUsername;
     String Username;
 
+    //Untuk Define Menu Samping
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    //Untuk Define Menu Samping
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -23,11 +30,28 @@ public class LayarUtama extends AppCompatActivity {
 
         //TVUsername = (TextView) findViewById(R.id.TVUsername);
 
+        //Untuk Define Menu Samping
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //Untuk Define Menu Samping
+
         Intent intent = getIntent();
         Username = intent.getStringExtra("Login");
 
-        //TVUsername.setText("Welcome, " + Username);
-
     }
+
+    //Supaya tombol menu kiri atas bisa di klik
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    //Supaya tombol menu kiri atas bisa di klik
 
 }
--- app/src/main/java/com/iceman/itpsm/MainActivity.java
+++ app/src/main/java/com/iceman/itpsm/MainActivity.java
@@ -2,18 +2,12 @@ package com.iceman.itpsm;
 
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import android.widget.ToggleButton;
-
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -37,23 +31,10 @@ public class MainActivity extends AppCompatActivity {
     String MobilePhoneK;
     String PasswordK;
 
-    //Untuk Define Menu Samping
-    private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mToggle;
-    //Untuk Define Menu Samping
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Untuk Define Menu Samping
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
-        //mDrawerLayout.addDrawerListener(mToggle);
-        //mToggle.syncState();
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        //Untuk Define Menu Samping
 
         ETUsername = (EditText) findViewById(R.id.ETUsername);
         ETPassword = (EditText) findViewById(R.id.ETPassword);
@@ -61,17 +42,6 @@ public class MainActivity extends AppCompatActivity {
 
         koneksi = new ConnectionClass();
     }
-
-    //Supaya tombol menu kiri atas bisa di klik
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        if(mToggle.onOptionsItemSelected(item))
-//        {
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
-    //Supaya tombol menu kiri atas bisa di klik
 
     public void login(View view) {
 
@@ -109,7 +79,6 @@ public class MainActivity extends AppCompatActivity {
                         "where ID =" + "'" + username + "'";
 
                 Statement stmt = null;
-                Log.d("Disini", querylogin);
 
                 try {
                     stmt = kon.createStatement();
