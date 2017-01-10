3f69bb3af9c1f1fb2c4eccfaaaed6fb06897f2d7
jktmuhip <boskovic234@gmail.com>
2017-01-10 16:44:18 +0700

    Update Kantor 4, perbaikan drawer, navigation masih error

--- app/src/main/java/com/iceman/itpsm/LayarUtama.java
+++ app/src/main/java/com/iceman/itpsm/LayarUtama.java
@@ -2,12 +2,15 @@ package com.iceman.itpsm;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Boskovic on 12/25/2016.
@@ -31,13 +34,16 @@ public class LayarUtama extends AppCompatActivity {
 
         TVUsername = (TextView) findViewById(R.id.TVName);
 
-        //Untuk Define Menu Samping
+        //MENU DRAWER
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        //Untuk Define Menu Samping
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view) ;
+        navigationView.setNavigationItemSelectedListener(this);
+        //MENU DRAWER
 
         Intent intent = getIntent();
         Username = intent.getStringExtra("Nama");
@@ -57,5 +63,25 @@ public class LayarUtama extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
     //Supaya tombol menu kiri atas bisa di klik
+
+    public boolean onNavigationItemSelected (MenuItem item)
+    {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_logout)
+        {
+            Toast.makeText(this, "LOGOUT TRUE", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(this, "LOGOUT FALSE", Toast.LENGTH_SHORT).show();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerLayout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
 
 }
